c1a5ce595b46fceaa4f1556142877c3fc45c2730
cindysurjawann <csurjawan@gmail.com>
2024-05-20 15:37:13 +0700

    add page admin all reservation & migration for kolom processed in table reservations

--- internal/render/render.go
+++ internal/render/render.go
@@ -7,19 +7,28 @@ import (
 	"net/http"
 	"path/filepath"
 	"text/template"
+	"time"
 
 	"github.com/cindysurjawann/gobookings/internal/config"
 	"github.com/cindysurjawann/gobookings/internal/models"
 	"github.com/justinas/nosurf"
 )
 
-var functions = template.FuncMap{}
+// functions that available to my golang template
+var functions = template.FuncMap{
+	"humanDate": HumanDate,
+}
 var app *config.AppConfig
 var pathToTemplates = "./templates"
 
 // sets the config for the template package
 func NewRenderer(a *config.AppConfig) {
 	app = a
+}
+
+// returns time in YYY-MM-DD format
+func HumanDate(t time.Time) string {
+	return t.Format("2006-01-02")
 }
 
 func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
